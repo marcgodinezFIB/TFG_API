a98c52ee6c03af56a4cf03dab743a0ff8293753f
marcgodinez <marcgodinez98@gmail.com>
2022-05-19 16:05:10 +0200

    multer changes

--- routes/index.js
+++ routes/index.js
@@ -27,7 +27,7 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb){
-        cb(null, '../api/uploads/');
+        cb(null, 'https://master--clinquant-pudding-e02756.netlify.app/api/uploads/');
     },
     filename: function(req, file, cb){
         cb(null,file.originalname);
